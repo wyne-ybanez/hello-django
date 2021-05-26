8fb0f5657edf9b216c480f70c7e96eed3298ada3
wyne-ybanez <wyne_01@icloud.com>
2021-05-26 16:32:37 +0000

    Add test case for getting todo list

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,7 +1,24 @@
 from django.test import TestCase
 
 
-class TestDjango(TestCase):
+class TestViews(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+    def test_get_todo_list(self):
+        """ 
+        Getting client response for home page.
+        Ensure it's status 200, a successful response.
+        Tell it what template we expect to be used in response.
+        """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+    
+    # def test_get_add_item_page(self):
+
+    # def test_get_edit_item_page(self):
+
+    # def test_can_add_item(self):
+
+    # def test_can_delete_item(self):
+
+    # def test_can_toggle_item(self):
