eab570485bb697714f61c297abae17828b87fd4f
wyne-ybanez <wyne_01@icloud.com>
2021-05-26 11:35:52 +0000

    Bug with local deployment discovered - app not running - troubleshooting still on going

--- todo/views.py
+++ todo/views.py
@@ -33,7 +33,7 @@ def add_item(request):
 
 def edit_item(request, item_id):
     """
-    Edits Item 
+    Edits item 
     """
     item = get_object_or_404(Item, id=item_id)
     if request.method == 'POST':
@@ -52,4 +52,11 @@ def toggle_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.done = not item.done
     item.save()
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    
     return redirect('get_todo_list')
